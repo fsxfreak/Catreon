c91c5f9153cb80164341d41e2ee85fd01f2dc3bb
Leon Cheung <me@leoncheung.com>
2012-04-26 17:10:45 -0700

    more gamestate

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -69,3 +69,95 @@ void GameState::exit()
 	if (mSceneMgr)
 		OgreFramework::getSingletonPtr()->mRoot->destroySceneManager(mSceneMgr);
 }
+//-------------------------------------------------------------------------------------------------------
+//inherited from Appstate, fill the scene
+void GameState::createScene()
+{
+	mSceneMgr->createLight("Light")->setPosition(0, 75, 30);
+
+	DotSceneLoader* pDotSceneLoader = new DotSceneLoader();
+	pDotSceneLoader->parseDotScene("CubeScene.xml", "General", mSceneMgr, mSceneMgr->getRootSceneNode());
+	delete pDotSceneLoader;
+
+	mSceneMgr->getEntity("Cube01")->setQueryFlags(CUBE_MASK);
+	mSceneMgr->getEntity("Cube02")->setQueryFlags(CUBE_MASK);
+	mSceneMgr->getEntity("Cube03")->setQueryFlags(CUBE_MASK);
+
+	mOgreHeadEntity = mSceneMgr->createEntity("Cube", "ogrehead.mesh");
+	mOgreHeadEntity->setQueryFlags(OGRE_HEAD_MASK);
+	mOgreHeadNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("CubeNode");
+	mOgreHeadNode->attachObject(mOgreHeadEntity);
+	mOgreHeadNode->setPosition(Ogre::Vector3(0, 0, -15));
+
+	mOgreHeadMaterial = mOgreHeadEntity->getSubEntity(1)->getMaterial();
+	mOgreHeadMaterialHigh = mOgreHeadMaterial->clone("OgreHeadMaterialHigh");
+	mOgreHeadMaterialHigh->getTechnique(0)->getPass(0)->setAmbient(1, 0, 0);
+	mOgreHeadMaterialHigh->getTechnique(0)->getPass(0)->setDiffuse(1, 0 ,0 , 0);
+}
+//-------------------------------------------------------------------------------------------------------
+bool GameState::keyPressed(const OIS::KeyEvent &keyEvent)
+{
+	if (mbSettingsMode == true)
+	{
+		if (OgreFramework::getSingletonPtr()->mKb->isKeyDown(OIS::KC_S))
+		{
+			//cycle up the settings menu toggled by TAB
+			OgreBites::SelectMenu* pMenu = (OgreBites::SelectMenu*)OgreFramework::getSingletonPtr()->mTrayMgr->getWidget("ChatModeSelMenu");
+			if (pMenu->getSelectionIndex() + 1 < static_cast<int>(pMenu->getNumItems()))
+			{
+				pMenu->selectItem(pMenu->getSelectionIndex() + 1);
+			}
+		}
+		if (OgreFramework::getSingletonPtr()->mKb->isKeyDown(OIS::KC_W))
+		{
+			OgreBites::SelectMenu* pMenu = (OgreBites::SelectMenu*)OgreFramework::getSingletonPtr()->mTrayMgr->getWidget("ChatModeSelMenu");
+			if (pMenu->getSelectionIndex() - 1 >= 0)
+			{
+				pMenu->selectItem(pMenu->getSelectionIndex() - 1);
+			}
+		}
+	}
+
+	//pause the state
+	if (OgreFramework::getSingletonPtr()->mKb->isKeyDown(OIS::KC_ESCAPE))
+	{
+		pushAppState(findByName("PauseState"));
+		return true;
+	}
+
+	//toggle information
+	if (OgreFramework::getSingletonPtr()->mKb->isKeyDown(OIS::KC_I))
+	{
+		if (mDetailsPanel->getTrayLocation() == OgreBites::TL_NONE)
+		{
+			OgreFramework::getSingletonPtr()->mTrayMgr->moveWidgetToTray(mDetailsPanel, OgreBites::TL_TOPLEFT, 0);
+			mDetailsPanel->show();
+		}
+		else
+		{
+			OgreFramework::getSingletonPtr()->mTrayMgr->removeWidgetFromTray(mDetailsPanel);
+			mDetailsPanel->hide();
+		}
+	}
+
+	if (OgreFramework::getSingletonPtr()->mKb->isKeyDown(OIS::KC_TAB))
+	{
+		mbSettingsMode = !mbSettingsMode;
+		return true;
+	}
+
+	//on press enter
+	if (mbSettingsMode && OgreFramework::getSingletonPtr()->mKb->isKeyDown(OIS::KC_RETURN) ||
+		OgreFramework::getSingletonPtr()->mKb->isKeyDown(OIS::KC_NUMPADENTER))
+	{
+
+	}
+
+	//if not in settings mode (tab), or in settings mode and key isnt O, pass the keyevent to OgreFramework
+	if (!mbSettingsMode || (mbSettingsMode && !OgreFramework::getSingletonPtr()->mKb->isKeyDown(OIS::KC_O)))
+	{
+		OgreFramework::getSingletonPtr()->keyPressed(keyEvent);
+	}
+
+	return true;
+}
