f7b0b2c2d9c18b542db15e60ed3a7bee2d96e6ed
Leon Cheung <me@leoncheung.com>
2012-04-28 17:17:23 -0700

    finishing DemoApp, completed AdvancedOgreFramework

---
+++ src/DemoApp.cpp
@@ -0,0 +1,38 @@
+//code largely based on Advanced Ogre Framework at ogre3d.org
+//minor formatting and name changes to fit my conventions
+
+#include "stdafx.h"
+
+#include "DemoApp.hpp"
+
+#include "MenuState.hpp"
+#include "GameState.hpp"
+#include "PauseState.hpp"
+
+DemoApp::DemoApp() : mAppStateManager(0)
+{
+}
+//-------------------------------------------------------------------------------------------------------
+DemoApp::~DemoApp()
+{
+	delete mAppStateManager;
+	delete OgreFramework::getSingletonPtr();
+}
+//-------------------------------------------------------------------------------------------------------
+void DemoApp::startDemo()
+{
+	new OgreFramework();
+
+	if (!OgreFramework::getSingletonPtr()->initOgre("Catreon", 0, 0))
+		return;
+
+	OgreFramework::getSingletonPtr()->mLog->logMessage("Catreon initialized!");
+
+	mAppStateManager = new AppStateManager();
+
+	MenuState::create(mAppStateManager, "MenuState");
+	GameState::create(mAppStateManager, "GameState");
+	PauseState::create(mAppStateManager, "PauseState");
+
+	mAppStateManager->start(mAppStateManager->findByName("MenuState"));
+}
---
+++ src/DemoApp.hpp
@@ -0,0 +1,23 @@
+//code largely based on Advanced Ogre Framework at ogre3d.org
+//minor formatting and name changes to fit my conventions
+
+#ifndef DEMO_APP_HPP
+#define DEMO_APP_HPP
+
+#include "stdafx.h"
+#include "AdvancedOgreFramework.hpp"
+#include "AppStateManager.hpp"
+
+class DemoApp
+{
+public:
+	DemoApp();
+	~DemoApp();
+
+	void startDemo();
+private:
+	AppStateManager *mAppStateManager;
+};
+
+
+#endif
