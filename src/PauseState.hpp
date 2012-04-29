da4fbc8e0053c771ca5d149374da52833c8f423c
Leon Cheung <me@leoncheung.com>
2012-04-28 17:14:51 -0700

    completing PauseState

---
+++ src/PauseState.hpp
@@ -0,0 +1,37 @@
+//code largely based on Advanced Ogre Framework at ogre3d.org
+//minor formatting and name changes to fit my conventions
+
+#ifndef PAUSE_STATE_HPP
+#define PAUSE_STATE_HPP
+
+#include "stdafx.h"
+#include "AppState.hpp"
+
+class PauseState : public AppState
+{
+public:
+	PauseState();
+
+	DECLARE_APPSTATE_CLASS(PauseState)
+
+	void enter();
+	void createScene();
+	void exit();
+
+	bool keyPressed(const OIS::KeyEvent &keyEvent);
+	bool keyReleased(const OIS::KeyEvent &keyEvent);
+
+	bool mouseMoved(const OIS::MouseEvent &mouseEvent);
+	bool mousePressed(const OIS::MouseEvent &mouseEvent, OIS::MouseButtonID id);
+	bool mouseReleased(const OIS::MouseEvent &mouseEvent, OIS::MouseButtonID id);
+
+	void buttonHit(OgreBites::Button *button);
+	void yesNoDialogClosed(const Ogre::DisplayString &question, bool yesHit);
+
+	void update(double timeSinceLastFrame);
+
+private:
+	bool mbQuit;
+	bool mbQuestionActive;
+};
+#endif
