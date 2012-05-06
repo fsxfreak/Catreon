91e23f66539b0c58463814250e8aede26476d0c3
Leon Cheung <me@leoncheung.com>
2012-05-06 11:04:50 -0700

    creating vehicle helper class to manage speed, position, mesh, etc. of vehicle

---
+++ src/VehicleHelper.h
@@ -0,0 +1,34 @@
+/*
+author: Leon Cheung <me[at]leoncheung.com>
+Copyright (C) 2012
+
+This program is released under the terms of
+GNU Lesser General Public License version 3.0
+available at http://www.gnu.org/licenses/lgpl-3.0.txt
+*/
+
+#ifndef VEHICLE_HELPER_H
+#define VEHICLE_HELPER_H
+
+#include "stdafx.h"
+#include <vector>
+#include "Vehicle.h"
+
+class VehicleHelper
+{
+private:
+	std::vector<Vehicle*> mVehicles;
+
+	//car model
+	Ogre::Entity *mVehicleEntity;
+	Ogre::SceneNode *mVehicleNode;
+
+public:
+	VehicleHelper();
+	~VehicleHelper();
+
+	void addVehicle(const Vehicle& vehicle);
+
+};
+
+#endif
