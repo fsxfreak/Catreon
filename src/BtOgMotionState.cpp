8cf7fe0f8f433e58d2d45013bd74ae2e6aef6805
Leon Cheung <me@leoncheung.com>
2012-05-13 15:12:42 -0700

    tying motionstates with ogre scenenodes

---
+++ src/BtOgMotionState.cpp
@@ -0,0 +1,47 @@
+/*
+author: Leon Cheung <me[at]leoncheung.com>
+Copyright (C) 2012
+
+This program is released under the terms of
+GNU Lesser General Public License version 3.0
+available at http://www.gnu.org/licenses/lgpl-3.0.txt
+*/
+
+/********************************************************
+TODO
+
+********************************************************/
+
+#include "stdafx.h"
+#include "BtOgMotionState.h"
+
+BtOgMotionState::BtOgMotionState(const btTransform &initialposition, Ogre::SceneNode *node)
+{
+    mObject = node;
+    mPosition = initialposition;
+}
+//-------------------------------------------------------------------------------------------------------
+BtOgMotionState::~BtOgMotionState()
+{
+
+}
+//-------------------------------------------------------------------------------------------------------
+void BtOgMotionState::setNode(Ogre::SceneNode *node)
+{
+    mObject = node;
+}
+//-------------------------------------------------------------------------------------------------------
+void BtOgMotionState::getWorldTransform(btTransform &worldTransform) const
+{
+    worldTransform = mPosition;
+}
+//-------------------------------------------------------------------------------------------------------
+void BtOgMotionState::setWorldTransform(const btTransform &worldTransform)
+{
+    if (mObject == NULL)
+        return;
+    btQuaternion rotation = worldTransform.getRotation();
+    mObject->setOrientation(rotation.w(), rotation.x(), rotation.y(), rotation.z());
+    btVector3 position = worldTransform.getOrigin();
+    mObject->setPosition(position.x(), position.y(), position.z());
+}
---
+++ src/BtOgMotionState.h
@@ -0,0 +1,40 @@
+/*
+author: Leon Cheung <me[at]leoncheung.com>
+Copyright (C) 2012
+
+This program is released under the terms of
+GNU Lesser General Public License version 3.0
+available at http://www.gnu.org/licenses/lgpl-3.0.txt
+*/
+
+#ifndef BT_OG_MOTIONSTATE_H
+#define BT_OG_MOTIONSTATE_H
+
+/********************************************************
+TODO
+
+********************************************************/
+
+#include "stdafx.h"
+#include "btBulletCollisionCommon.h"
+#include "btBulletDynamicsCommon.h"
+#include "AdvancedOgreFramework.hpp"
+
+class BtOgMotionState : public btMotionState
+{
+public:
+    BtOgMotionState(const btTransform &initialPosition, Ogre::SceneNode *node);
+    virtual ~BtOgMotionState();
+
+    inline void setNode(Ogre::SceneNode *node);
+
+    virtual void getWorldTransform(btTransform &worldTransform) const;
+
+    virtual void setWorldTransform(const btTransform &worldTransform);
+    
+protected:
+    Ogre::SceneNode *mObject;
+    btTransform mPosition;
+};
+
+#endif
