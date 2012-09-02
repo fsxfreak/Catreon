33a2053db43204f4285fa685246e0d6b66845c28
Leon Cheung <me@leoncheung.com>
2012-09-02 11:43:00 -0700

    adding bullet debug drawer from LiMuBei

---
+++ src/framework/DebugDraw.hpp
@@ -0,0 +1,50 @@
+//All credit to LiMuBei and his team of Ogre3D forums
+
+#ifndef DEBUG_DRAW_HPP
+#define DEBUG_DRAW_HPP
+
+#include <btBulletDynamicsCommon.h>
+#include <OGRE/Ogre.h>
+
+#include "DynamicLineDrawer.hpp"
+
+/** Implements the bullet debug draw interface for drawing with ogre.
+	\ingroup Misc
+*/
+class CDebugDraw :
+	public btIDebugDraw
+{
+public:
+	CDebugDraw( Ogre::SceneManager* Scene, btDynamicsWorld* World );
+	~CDebugDraw();
+
+	/** Moves the data from the world to the ogre object.
+		Should be called once every frame.
+	*/
+	void		Update();
+
+protected:
+	void setDebugMode( int DebugMode );
+	int getDebugMode() const;
+
+	// these implement the debug/drawing interface, no need to call them from the outside
+	void drawLine( const btVector3& from, const btVector3& to, const btVector3& color );
+	void drawContactPoint( const btVector3& PointOnB, const btVector3& normalOnB,
+		btScalar distance, int lifeTime, const btVector3& color );
+	void reportErrorWarning( const char *warningString );
+	void draw3dText( const btVector3& location, const char* textString );
+	void drawSphere( const btVector3& p, btScalar radius, const btVector3& color );
+	void drawTriangle( const btVector3&v0, const btVector3 &v1, const btVector3 &v2,
+					   const btVector3 &color, btScalar alpha );
+
+
+private:
+	std::vector<Ogre::Vector2> UnitCircle;
+
+	CDynamicLineDrawer* DebugLineDrawer;
+	Ogre::SceneNode*	DebugDrawerNode;
+	btDynamicsWorld*	World;
+	bool				IsActive;
+};
+
+#endif
