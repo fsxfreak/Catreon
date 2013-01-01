6b64651b904f0e2efc12f30bdd610b3a89635bba
Leon Cheung <me@leoncheung.com>
2013-01-01 15:44:31 -0800

    init commit for Road class for pathfinding

---
+++ src/objects/Road.hpp
@@ -0,0 +1,20 @@
+/*
+author: Leon Cheung <me[at]leoncheung.com>
+Copyright (C) 2012
+
+This program is released under the terms of
+GNU Lesser General Public License version 3.0
+available at http://www.gnu.org/licenses/lgpl-3.0.txt
+*/
+
+#ifndef ROAD_HPP
+#define ROAD_HPP
+
+#include "stdafx.h"
+
+class Road
+{
+
+};
+
+#endif
