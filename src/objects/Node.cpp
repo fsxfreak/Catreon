959389edc2aeec2da1a5cd467266f50b1be16164
Leon Cheung <me@leoncheung.com>
2013-03-10 18:13:02 -0700

    Reducing responsibility of Road class

---
+++ src/objects/Node.cpp
@@ -0,0 +1,13 @@
+/*
+author: Leon Cheung <me[at]leoncheung.com>
+Copyright (C) 2012
+
+This program is released under the terms of
+GNU Lesser General Public License version 3.0
+available at http://www.gnu.org/licenses/lgpl-3.0.txt
+*/
+
+#include "stdafx.h"
+
+#include <objects\Node.hpp>
+
---
+++ src/objects/Node.hpp
@@ -0,0 +1,23 @@
+/*
+author: Leon Cheung <me[at]leoncheung.com>
+Copyright (C) 2012
+
+This program is released under the terms of
+GNU Lesser General Public License version 3.0
+available at http://www.gnu.org/licenses/lgpl-3.0.txt
+*/
+
+#ifndef NODE_HPP
+#define NODE_HPP
+
+#include "stdafx.h"
+
+#include <GameState.hpp>
+#include <objects\Road.hpp>
+
+class Node
+{
+
+};
+
+#endif
